ff4cd72d76068d987575ca156f2ca3f1060739cc
Jamal Muslimov <mechanoid0415@gmail.com>
2022-07-17 16:07:56 +0300

    refactor: remove commented code

--- src/index.js
+++ src/index.js
@@ -2,7 +2,3 @@
 module.exports = function towelSort (matrix) {
   return arguments.length ? [].concat(...matrix.map((arr, i) => i % 2 === 0 ? arr : arr.reverse())) : []
 }
-
-// function towelSort (matrix){
-//   return [].concat(...matrix.map((arr, i) => i % 2 === 0 ? arr : arr.reverse()))
-// }

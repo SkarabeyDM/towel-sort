2a5b1b55a8f066313fbaa78bf76e5f60fbd39c82
Jamal Muslimov <mechanoid0415@gmail.com>
2022-07-17 23:05:22 +0300

    refactor: small changes

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-
-module.exports = function towelSort (matrix) {
+function towelSort (matrix) {
   return arguments.length ? [].concat(...matrix.map((arr, i) => i % 2 === 0 ? arr : arr.reverse())) : []
 }
+
+module.exports = towelSort

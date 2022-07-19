1decf0f8b016cbc16117a1f3ff7351446da2e98e
Jamal Muslimov <mechanoid0415@gmail.com>
2022-07-19 12:38:56 +0300

    fix: add default parameter value

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
-const towelSort = (matrix) => arguments.length ? [].concat(...matrix.map((arr, i) => i % 2 === 0 ? arr : arr.reverse())) : [];
+const towelSort = (matrix = []) => [].concat(...matrix.map((arr, i) => i % 2 === 0 ? arr : arr.reverse()));
 
 module.exports = towelSort

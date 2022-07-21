80ed637d0507a67ce28dbd7c814c1f833fdd6ff5
Jamal Muslimov <mechanoid0415@gmail.com>
2022-07-21 19:47:46 +0300

    refactor: make code shorter

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
-const towelSort = (matrix = []) => [].concat(...matrix.map((arr, i) => i % 2 === 0 ? arr : arr.reverse()));
+//const towelSort = (matrix = []) => [].concat(...matrix.map((arr, i) => i % 2 === 0 ? arr : arr.reverse()));
+const towelSort = (matrix = []) => matrix.flatMap((arr, i) => i % 2 === 0 ? arr : arr.reverse())
 
 module.exports = towelSort

91d2aa90ef3875e49b6db4169c42acac48e731a4
Skarabey DM <mechanoid0415@gmail.com>
2023-02-11 12:37:21 +0300

    refactor: flip ternary operator

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-//const towelSort = (matrix = []) => [].concat(...matrix.map((arr, i) => i % 2 === 0 ? arr : arr.reverse()));
-const towelSort = (matrix = []) => matrix.flatMap((arr, i) => i % 2 === 0 ? arr : arr.reverse())
+const towelSort = (matrix = []) => matrix.flatMap((arr, i) => i % 2 ? arr.reverse() : arr)
 
 module.exports = towelSort
